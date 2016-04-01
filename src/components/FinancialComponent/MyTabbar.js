f274e3265aaa55c367dc77efea021fc10d258321
yangyongxiang <yangyong_xiang@126.com>
2016-04-01 13:50:45 +0800

    修改

--- src/components/FinancialComponent/MyTabbar.js
+++ src/components/FinancialComponent/MyTabbar.js
@@ -16,12 +16,9 @@ var {
     } = React;
 
 var heightTabRow = (Dimensions.get('window').height/13);
+var globalNavigator=null;
+
 var MyTabbar = React.createClass({
-    getInitialState: function() {
-        return {
-            tabIndex: 0,
-        };
-    },
     componentWillMount:function() {
         var markArray=[];
         var routerArray=[];
@@ -30,7 +27,51 @@ var MyTabbar = React.createClass({
         }
         this._routerArray = routerArray;
         this._markArray = markArray;
+    },
+    componentDidMount: function() {
 
+    },
+    componentWillUnmount:function() {
+    },
+    _configureScene () {
+        return Navigator.SceneConfigs.FadeAndroid;
+    },
+
+    _renderScene (router, navigator) {
+        var Component = null;
+        globalNavigator = navigator;
+        this._markArray[router.name]+=1;
+        console.log( router.name + " _markArray:" + this._markArray[router.name]);
+        this._routerArray[router.name]=router;
+        return this.props.tabArray[router.name].componentScene;
+    },
+
+    render() {
+        console.log("render" );
+        return(
+            <View>
+                <MyTabbarHeader tabArray={this.props.tabArray} routerArray={this._routerArray} markArray={this._markArray} />
+                <View style={{height:(this.props.height - heightTabRow)}}>
+                    <Navigator
+                        initialRoute={{name: 0}}
+                        configureScene={this._configureScene}
+                        renderScene={this._renderScene}
+                        />
+                </View>
+            </View>
+        )
+    }
+});
+
+var MyTabbarHeader = React.createClass({
+    getInitialState: function() {
+        return {
+            tabIndex: 0,
+        };
+    },
+    componentWillMount:function() {
+        this._routerArray = this.props.routerArray;
+        this._markArray = this.props.markArray;
     },
     componentDidMount: function() {
 
@@ -55,7 +96,7 @@ var MyTabbar = React.createClass({
         };
     },
 
-    _renderTabHeader(tabArray,styles){
+    _renderTabHeader(){
         return this.props.tabArray.map(function (items, i) {
             return (
                 <TouchableOpacity key={i} onPress={()=>this._tabSelectedEvent(i)} style={{flex: 1}}>
@@ -75,54 +116,34 @@ var MyTabbar = React.createClass({
         if(1>this._markArray[i]){
             console.log("this._navigator.push({name:i}); " + i);
             if(this._topRouter){
-                this._navigator.jumpTo(this._topRouter);
+                globalNavigator.jumpTo(this._topRouter);
             }
             console.log("this._topRouter; " + this._topRouter);
             this._topRouter = {name:i};
-            this._navigator.push(this._topRouter);
+            console.log("this._navigator; " + globalNavigator);
+            globalNavigator.push(this._topRouter);
         }else{
             console.log("this._navigator.jump({name:i});" + i);
-            this._navigator.jumpTo(this._routerArray[i]);
+            globalNavigator.jumpTo(this._routerArray[i]);
         }
         console.log("pre setState:" + this.state.tabIndex);
         this.setState({tabIndex: i});
         console.log("after setState:" + this.state.tabIndex);
     },
 
-    _configureScene () {
-        return Navigator.SceneConfigs.FadeAndroid;
-    },
-
-    _renderScene (router, navigator) {
-        var Component = null;
-        this._navigator = navigator;
-        this._markArray[router.name]+=1;
-        console.log( router.name + " _markArray:" + this._markArray[router.name]);
-        this._routerArray[router.name]=router;
-        return this.props.tabArray[router.name].componentScene;
-    },
-
     render() {
-        console.log("render" );
+        console.log("MyTabbarHeader render" );
         return(
-            <View>
                 <View style={styles.flexContainer}>
                     <ScrollView key={'scrollView'} horizontal={true}>
-                        { this._renderTabHeader(this.props.tabArray,styles)}
+                        { this._renderTabHeader()}
                     </ScrollView>
-
                 </View>
-                <View style={{height:(this.props.height - heightTabRow)}}>
-                    <Navigator
-                        initialRoute={{name: 0}}
-                        configureScene={this._configureScene}
-                        renderScene={this._renderScene}
-                        />
-                </View>
-            </View>
         )
     }
 });
+
+
 module.exports=MyTabbar;
 var styles = StyleSheet.create({
     flexContainer: {
