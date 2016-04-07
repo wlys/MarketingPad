43b824e87e3bc5eae8102c2c73a5213b185de99f
shilonggang <shilonggang@gmail.com>
2016-04-07 17:54:54 +0800

    modify homePage's showlist ,using flex

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -7,15 +7,18 @@ var {
     } = React;
 
 const WINDOW_HEIGHT = (Dimensions.get('window').height);
-const NAVBAR_HEIGHT = 55 ;
+const NAVBAR_HEIGHT = 50;
 const TABBAR_HEIGHT = 60;
 
 const WINDOW_WIDTH = (Dimensions.get('window').width);
+
+const MARGIN_HEIGHT =5;
 
 
 module.exports = {
   WINDOW_HEIGHT,
   WINDOW_WIDTH,
   NAVBAR_HEIGHT,
-  TABBAR_HEIGHT
+  TABBAR_HEIGHT,
+  MARGIN_HEIGHT
 };
